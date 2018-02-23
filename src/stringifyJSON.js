e60c19096be901ac67785d00329796e152dac2bc
Michael Ye <micxye@yahoo.com>
2018-02-23 02:57:03 -0800

    Implemented stringifyJSON to work like JSON.stringify. Passes all mocha tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,47 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"'
+  }
+  if (typeof obj === 'number') {
+    return obj.toString()
+  }
+  if (obj === null) {
+    return 'null'
+  }
+  if (typeof obj !== 'object' && obj === true) {
+    return 'true'
+  }
+  if (typeof obj !== 'object' && obj === false) {
+    return 'false'
+  }
+  var copy = obj
+  var retval = ''
+  if (Array.isArray(copy)) {
+    if (copy.length === 0) {
+      return '[]'
+    }
+    for (var i = 0; i < copy.length; i++) {
+      if (i === copy.length-1) {
+        return '[' + retval + stringifyJSON(copy[i]) + ']'
+      }
+      retval += stringifyJSON(copy[i]) + ','
+    }
+  }
+  if (typeof obj === 'object') {
+    var objectkeys = Object.keys(obj)
+    if (objectkeys.length === 0) {
+      return '{}'
+    }
+    for (key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        return '{}'
+      }
+      if (key === objectkeys[objectkeys.length-1]) { //if last key
+        return '{' + retval + stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + '}'
+      }
+      retval += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) +','
+    }
+  }
 };
